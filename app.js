f92afd7be656a619145190828a39a50dd6331d78
Prathibha Ratnayake <prsthibha@gmail.com>
2020-08-31 13:41:51 +0530

    dummy campgrounds added

--- app.js
+++ app.js
@@ -4,12 +4,30 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+let campgrounds = [
+  {
+    name: "Salmon Creek",
+    image:
+      "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=350",
+  },
+  {
+    name: "Granite Hill",
+    image:
+      "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350",
+  },
+  {
+    name: "Cloud's Rest",
+    image:
+      "https://images.pexels.com/photos/1230302/pexels-photo-1230302.jpeg?auto=compress&cs=tinysrgb&h=350",
+  },
+];
+
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
 app.get("/campgrounds", (req, res) => {
-  res.render("campgrounds");
+  res.render("campgrounds", {campgrounds:campgrounds});
 });
 
 app.listen("3000", (req, res) => {
