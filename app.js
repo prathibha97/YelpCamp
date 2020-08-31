93774a1658e211d9690a06b7dd5b42d5cc1fa2f2
Prathibha Ratnayake <prsthibha@gmail.com>
2020-08-31 14:26:34 +0530

    updated post route

--- app.js
+++ app.js
@@ -1,39 +1,52 @@
 const express = require("express");
-const { Router } = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
 let campgrounds = [
-  {
-    name: "Salmon Creek",
-    image:
-      "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=350",
-  },
-  {
-    name: "Granite Hill",
-    image:
-      "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=350",
-  },
-  {
-    name: "Cloud's Rest",
-    image:
-      "https://images.pexels.com/photos/1230302/pexels-photo-1230302.jpeg?auto=compress&cs=tinysrgb&h=350",
-  },
-];
+    {
+      name: "salmon Creek",
+      image:
+        "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=350",
+    },
+    {
+      name: "Granite Hill",
+      image:
+        "https://images.pexels.com/photos/1230302/pexels-photo-1230302.jpeg?auto=compress&cs=tinysrgb&h=350",
+    },
+    {
+      name: "Mountain Goats Rest",
+      image:
+        "https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&h=350",
+    },
+  ];
 
 app.get("/", (req, res) => {
-  res.render("landing");
+  res.render("Landing");
 });
 
 app.get("/campgrounds", (req, res) => {
   res.render("campgrounds", { campgrounds: campgrounds });
 });
 
+app.post("/campgrounds", (req, res) => {
+  // get data from form and add to camgrounds array
+  const name = req.body.name;
+  const image = req.body.image;
+  const newCampground = {name:name, image:image};
+
+  campgrounds.push(newCampground);
+  
+  // redirect back to campgrounds page
+  res.redirect("/campgrounds");
+});
+
 app.get("/campgrounds/new", (req, res) => {
-  res.render("new");
+  res.render("new.ejs");
 });
 
 app.listen("3000", (req, res) => {
-  console.log("Server started on port 3000");
+  console.log("YelpCamp Server has started");
 });
