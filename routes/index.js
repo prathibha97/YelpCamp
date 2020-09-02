527e206fbed25b74d25ad8091cc6b52ee7ca1e21
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-02 10:01:26 +0530

    landing page style

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const User = require("../models/user");
-const user = require("../models/user");
 
 router.get("/", (req, res) => {
   res.render("Landing");
